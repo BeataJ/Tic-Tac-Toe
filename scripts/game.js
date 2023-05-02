4fcb56feec1d1cb7e068d80de2715b41ab89d8d0
beataj <beata@jasinski.com>
2023-05-01 21:00:09 -0700

    checking row column

--- scripts/game.js
+++ scripts/game.js
@@ -1,43 +1,64 @@
 function startNewGame() {
-    if(players[0].name === ''  || players[1].name === '') {
-        alert('Please set custom player names for both players!')
-        return;
-    }
+  if (players[0].name === "" || players[1].name === "") {
+    alert("Please set custom player names for both players!");
+    return;
+  }
 
-    activePlayerNameElement.textContent = players[activePlayer].name;
-    gameAreaElement.style.display = 'block';
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = "block";
 }
 
 function switchPlayer() {
-    if(activePlayer === 0) {
-        activePlayer = 1;
-    } else {
-        activePlayer = 0;
-    }
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
 
-    activePlayerNameElement.textContent = players[activePlayer].name;
+  activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
 function selectGameField(event) {
-    if(event.target.tagName !== 'LI') {
-        return;
+  if (event.target.tagName !== "LI") {
+    return;
+  }
+
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert("Please select an empty field!");
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+  console.log(gameData);
+
+  switchPlayer();
+}
+
+function checkForGameOver() {
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
     }
-    
-    const selectedField = event.target;
-    const selectedColumn = selectedField.dataset.col - 1;
-    const selectedRow = selectedField.dataset.row - 1;
+  }
 
-    if(gameData[selectedRow][selectedColumn] > 0) {
-        alert('Please select an empty field!')
-        return
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
     }
-
-    selectedField.textContent = players[activePlayer].symbol;
-    selectedField.classList.add('disabled');
-
-
-    gameData[selectedRow][selectedColumn] = activePlayer + 1;
-    console.log(gameData);
-
-    switchPlayer();
+  }
 }
