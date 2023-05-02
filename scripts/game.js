678c1abdb712fd09052d94a83aac5fce157f7666
beataj <beata@jasinski.com>
2023-05-01 23:54:58 -0700

    resetGameStatus()

--- scripts/game.js
+++ scripts/game.js
@@ -4,6 +4,18 @@ function resetGameStatus() {
     gameOverElement.firstElementChild.innerHTML =
       'You won, <span id="winner-name">PLAYER NAME</span>!';
       gameOverElement.style.display = 'none';
+
+    let gameBoardIndex = 0;
+    for(let i = 0; i < 3; i++) {
+        for(let j = 0; j < 3; j++) {
+            gameData[i][j] = 0;
+           const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+           gameBoardItemElement.textContent = '';
+           gameBoardItemElement.classList.remove('disabled');
+            gameBoardIndex++
+        }
+    }  
+
 }
 
 function startNewGame() {
